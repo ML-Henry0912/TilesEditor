af538fc98c7469bf1a8f47c8c34ba756fd838c9f
ML-Henry0912 <henry@medialand.tw>
2025-05-13 19:00:09 +0800

    no message

---
+++ Assets/Scripts/Gizmo/GizmoBase.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static TilesEditor.iGizmo;
+
+namespace TilesEditor
+{
+    public class GizmoBase : MonoBehaviour//, iGizmoBase
+    {
+        protected GizmoType type;
+
+        protected bool isHovered = false;
+        protected TransformGizmo gizmo;
+        protected Color baseColor;
+        protected MaterialPropertyBlock propertyBlock;
+        protected Camera cam;
+
+        private void OnMouseEnter()
+        {
+            isHovered = true;
+            SetMaterialColor(Color.yellow);
+        }
+
+        private void OnMouseExit()
+        {
+            isHovered = false;
+            SetMaterialColor(baseColor);
+        }
+
+        public void SetInvisible(bool value)
+        {
+            var renderer = GetComponent<MeshRenderer>();
+            if (renderer != null)
+            {
+                renderer.enabled = !value;
+            }
+        }
+
+        public bool IsHovered()
+        {
+            return isHovered;
+        }
+
+        public void SetMaterialColor(Color color)
+        {
+            if (propertyBlock == null)
+                propertyBlock = new MaterialPropertyBlock();
+            color.a = 0.8f; // 預設 80% 透明度
+            propertyBlock.SetColor("_Color", color);
+            var renderer = GetComponent<MeshRenderer>();
+            if (renderer != null)
+            {
+                renderer.SetPropertyBlock(propertyBlock);
+            }
+        }
+
+        public void ResetColor()
+        {
+            SetMaterialColor(baseColor);
+        }
+
+
+    }
+}
